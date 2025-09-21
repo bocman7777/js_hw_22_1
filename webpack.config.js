6d9ff14be286566da12a683ccef9da26ccac463b
bocman7777 <vanya.potulnyy.05@gmail.com>
2025-09-21 17:35:42 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,56 +5,70 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "bundle.[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-        publicPath: "./",
-    },
-    mode: "production",
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: "asset",
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            inject: "body",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "styles.[contenthash].css",
-        }),
-        new ImageMinimizerPlugin({
-            minimizer: {
-                implementation: ImageMinimizerPlugin.imageminMinify,
-                options: {
-                    plugins: [
-                        ["gifsicle", { interlaced: true }],
-                        ["jpegtran", { progressive: true }],
-                        ["optipng", { optimizationLevel: 5 }],
-                        ["svgo", {}],
+module.exports = (env, argv) => {
+    const isDev = argv.mode === "development";
+
+    return {
+        entry: "./index.js", // входной JS в корне
+        output: {
+            filename: "bundle.[contenthash].js",
+            path: path.resolve(__dirname, "dist"),
+            clean: true,
+            publicPath: "./", // важно для GitHub Pages
+        },
+        mode: isDev ? "development" : "production",
+        devtool: isDev ? "inline-source-map" : "source-map",
+        devServer: {
+            static: "./dist",
+            open: true,
+            hot: true,
+            watchFiles: ["./index.html", "./index.js", "./style.scss"], // вотчер
+        },
+        module: {
+            rules: [
+                // SCSS → CSS
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        isDev ? "style-loader" : MiniCssExtractPlugin.loader, // HMR в dev
+                        "css-loader",
+                        "sass-loader",
                     ],
                 },
-            },
-        }),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
-    },
+                // Оптимизация и вставка картинок
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/i,
+                    type: "asset",
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./index.html", // шаблон в корне
+                inject: "body",
+            }),
+            !isDev &&
+            new MiniCssExtractPlugin({
+                filename: "styles.[contenthash].css",
+            }),
+            !isDev &&
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        plugins: [
+                            ["gifsicle", { interlaced: true }],
+                            ["jpegtran", { progressive: true }],
+                            ["optipng", { optimizationLevel: 5 }],
+                            ["svgo", {}],
+                        ],
+                    },
+                },
+            }),
+        ].filter(Boolean),
+        optimization: {
+            minimize: !isDev,
+            minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+        },
+    };
 };
